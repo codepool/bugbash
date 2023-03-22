b22ae3fab32d950790948929c6897c1f6bda472f
Sahil <bits.sahil@gmail.com>
2023-03-22 11:04:47 +0530

    leaderboard api integration

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ app.get('/', (req, res) => {
   res.send('Hello World123!')
 })
 
-app.post('/', (req, res) => {
+app.post('/', async (req, res) => {
     console.log("Got hit from jira!!!")
-    axios.post("https://keepthescore.co/api/uhnckkbyhse/score", {
+    await axios.post("https://keepthescore.co/api/uhnckkbyhse/score", {
         "score": 3,
         "player_id": 6896395
     }) 
