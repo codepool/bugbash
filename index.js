fb8fbf53a004211547e85edf73c84a3c3b625206
Sahil <bits.sahil@gmail.com>
2023-03-22 12:11:40 +0530

    leaderboard api integration

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const app = express()
 app.use(express.json())
 const port = 3000
 
+
+const playerIdMap = {
+
+    "629da8781be00a0068ac0d84": 6867317,
+    "628f1d20f2ee4a0069e08f4b": 6867182,
+    "6238138fa95758006958e9d6": 6896395,
+    "5f893681acaa7100681fa022": 6896390
+
+};
 app.get('/', (req, res) => {
     console.log("Got hit!!!")
     
@@ -12,11 +21,14 @@ app.get('/', (req, res) => {
 
 app.post('/', async (req, res) => {
     console.log("Got hit from jira!!!")
-    /*await axios.post("https://keepthescore.co/api/uhnckkbyhse/score", {
-        "score": 3,
-        "player_id": 6896395
-    })*/
-    console.log(req.body.issue.fields.reporter.accountId)
+    //if(req.body.changelog[0].field=='priority')
+    const playerId = playerIdMap[req.body.issue.fields.reporter.accountId];
+    await axios.post("https://keepthescore.co/api/uhnckkbyhse/score", {
+        "score": 1,
+        "player_id": playerId
+    })
+    
+    
     res.send()
 })
 
