a3252b0579851000f4fa18bdd02f1c3b5e20925a
Sahil <bits.sahil@gmail.com>
2023-04-02 21:36:56 +0530

    fixes

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.post('/', async (req, res) => {
         res.send()
         return;
     }
-    if(req.body.changelog.items[0].field != 'Priority' || (req.body.issue.fields.customfield_10071 && req.body.issue.fields.customfield_10071 == 'Rejected')) {
+    if(req.body.changelog.items[0].field != 'Priority' || (req.body.issue.fields.customfield_10071 && req.body.issue.fields.customfield_10071.value == 'Rejected')) {
         res.send()
         return;
     }
